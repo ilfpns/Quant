7d4248f6b4abb0d154f91a109052bf481289c6b8
ilfpns <yeomsh09@gmail.com>
2026-09-17 00:46:56 +0900

    delete LSTM model

--- ptq/model.py
+++ ptq/model.py
@@ -5,17 +5,6 @@ from torch import nn
 from torch.ao.quantization import DeQuantStub, QuantStub
 
 
-class SimpleLSTM(nn.Module):
-    def __init__(self, input_size: int = 128, hidden_size: int = 256, output_size: int = 10) -> None:
-        super().__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        output, _ = self.lstm(x)
-        return self.fc(output[:, -1, :])
-
-
 class SimpleMLP(nn.Module):
     def __init__(self, input_size: int = 128, hidden_size: int = 256, output_size: int = 10) -> None:
         super().__init__()
